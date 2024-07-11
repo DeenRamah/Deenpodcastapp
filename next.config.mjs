6e065efb943be1530a6a7765414281bee2d3cc73
DeenRamah <deenramahh66@gmail.com>
2024-07-11 21:16:36 +0300

    first commit

---
+++ next.config.mjs
@@ -0,0 +1,24 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lovely-flamingo-139.convex.cloud'
+      },
+      {
+        protocol: 'https',
+        hostname: 'gregarious-axolotl-455.convex.cloud'
+      },
+      {
+        protocol: 'https',
+        hostname: 'img.clerk.com'
+      },
+    ]
+  }
+};
+
+export default nextConfig;
